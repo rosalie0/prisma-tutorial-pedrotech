39061d1488d4c84189af2d1991df3e4848d0024a
Rosalie Newman <rosalienewman0@gmail.com>
2023-07-09 13:19:38 -0400

    GET all houses, GET house by id, GET filtered houses, POST house

--- index.js
+++ index.js
@@ -49,4 +49,72 @@ app.delete("/:id", async (req, res) => {
   res.json(user);
 });
 
+// GET all houses
+app.get("/house", async (req, res) => {
+  const allHouses = await prisma.house.findMany({
+    // returns the owner and builtBy user objects
+    include: {
+      owner: true,
+      builtBy: true,
+    },
+  });
+  res.json(allHouses);
+});
+
+// Get houses with a wifipassword and the owner is 18+
+app.get("/house/withFilters", async (req, res) => {
+  const filteredHouses = await prisma.house.findMany({
+    //Get me the houses where...
+    where: {
+      wifiPassword: { not: null }, // there is a wifiPassword
+      owner: {
+        age: { gte: 18 }, // the owner's age is greater than or equal to 18
+      },
+    },
+    // Sort it A-Z of the owner's name.
+    orderBy: [{ owner: { firstName: "desc" } }],
+    // Include the owner and builtBy as objects.
+    include: {
+      owner: true,
+      builtBy: true,
+    },
+  });
+  console.log(filteredHouses);
+  res.json(filteredHouses);
+});
+// Get house of id
+app.get("/house/:id", async (req, res) => {
+  const { id } = req.params;
+  const allHouses = await prisma.house.findUnique(
+    {
+      where: { id },
+    },
+    {
+      // returns the owner and builtBy user objects
+      include: {
+        owner: true,
+        builtBy: true,
+      },
+    }
+  );
+  res.json(allHouses);
+});
+
+// POST house
+app.post("/house", async (req, res) => {
+  const { address, wifiPassword, ownerId, builtById } = req.body;
+  console.log(address, wifiPassword, ownerId, builtById);
+  try {
+    const newHouse = await prisma.house.create({
+      data: { address, wifiPassword, ownerId, builtById },
+    });
+    res.json(newHouse);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the user." });
+  }
+});
+
 app.listen(3001, () => console.log("express server running on port 3001..."));
