be93147a965ae0c3ee2c682dcf02d797dc616dfe
Rosalie Newman <rosalienewman0@gmail.com>
2023-07-09 11:53:44 -0400

    get and post reqs written, tested with postman.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
+
+// body parse
 app.use(express.json());
+
+// GET all users
+app.get("/", async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
+
+// POST user
+app.post("/", async (req, res) => {
+  const { firstName, lastName, age } = req.body;
+  console.log(firstName, lastName, age);
+  const newUser = await prisma.user.create({
+    data: { firstName, lastName, age },
+  });
+  res.json(newUser);
+});
 
 app.listen(3001, () => console.log("express server running on port 3001..."));
