7e9acdc0d78985a5ba87ffcbeb50207d24060fed
Rosalie Newman <rosalienewman0@gmail.com>
2023-07-09 12:04:03 -0400

    PUT and DELETE written

--- index.js
+++ index.js
@@ -23,4 +23,27 @@ app.post("/", async (req, res) => {
   res.json(newUser);
 });
 
+// PUT user (only their age)
+app.put("/:id", async (req, res) => {
+  //
+  const { id } = req.params;
+  const newAge = req.body.age;
+  // Update the user with the id of parseInt id, update their age.
+  const user = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: { age: newAge },
+  });
+  res.json(user);
+});
+
+app.delete("/:id", async (req, res) => {
+  //
+  const { id } = req.params;
+  // Update the user with the id of parseInt id, update their age.
+  const user = await prisma.user.delete({
+    where: { id: parseInt(id) },
+  });
+  res.json(user);
+});
+
 app.listen(3001, () => console.log("express server running on port 3001..."));
